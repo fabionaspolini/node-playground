6059e27d2d4d207ad0e779fced3487f7d6f68c4a
Fábio Naspolini <fabionaspolini@gmail.com>
2026-05-14 21:47:25 -0300

    geo-data-onion-api-gemini: fix routes

--- geografic-data-api/geo-data-onion-api-gemini/src/api/routes/cidade-routes.ts
+++ geografic-data-api/geo-data-onion-api-gemini/src/api/routes/cidade-routes.ts
@@ -9,10 +9,10 @@ const repository = new PrismaCidadeRepository();
 const useCase = new CidadeUseCase(repository, kafkaProducer);
 const controller = new CidadeController(useCase);
 
-router.post("/cidades/post", (req, res) => controller.create(req, res));
-router.get("/cidades/get/:id", (req, res) => controller.getById(req, res));
-router.get("/cidades/get", (req, res) => controller.list(req, res));
-router.put("/cidades/put/:id", (req, res) => controller.update(req, res));
-router.delete("/cidades/delete/:id", (req, res) => controller.delete(req, res));
+router.post("/cidades", (req, res) => controller.create(req, res));
+router.get("/cidades/:id", (req, res) => controller.getById(req, res));
+router.get("/cidades", (req, res) => controller.list(req, res));
+router.put("/cidades/:id", (req, res) => controller.update(req, res));
+router.delete("/cidades/:id", (req, res) => controller.delete(req, res));
 
 export default router;
--- geografic-data-api/geo-data-onion-api-gemini/src/api/routes/estado-routes.ts
+++ geografic-data-api/geo-data-onion-api-gemini/src/api/routes/estado-routes.ts
@@ -9,10 +9,10 @@ const repository = new PrismaEstadoRepository();
 const useCase = new EstadoUseCase(repository, kafkaProducer);
 const controller = new EstadoController(useCase);
 
-router.post("/estados/post", (req, res) => controller.create(req, res));
-router.get("/estados/get/:id", (req, res) => controller.getById(req, res));
-router.get("/estados/get", (req, res) => controller.list(req, res));
-router.put("/estados/put/:id", (req, res) => controller.update(req, res));
-router.delete("/estados/delete/:id", (req, res) => controller.delete(req, res));
+router.post("/estados", (req, res) => controller.create(req, res));
+router.get("/estados/:id", (req, res) => controller.getById(req, res));
+router.get("/estados", (req, res) => controller.list(req, res));
+router.put("/estados/:id", (req, res) => controller.update(req, res));
+router.delete("/estados/:id", (req, res) => controller.delete(req, res));
 
 export default router;
