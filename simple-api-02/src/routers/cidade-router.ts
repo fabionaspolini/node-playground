3afb7a773858be4ac6487b6b54b8c11ba0481d52
Fábio Naspolini <fabionaspolini@gmail.com>
2026-05-19 16:29:42 -0300

    simple-api-02: refactor standardize repository variable naming in cidadeRouter
    - Renamed `cidadeRepository` to `repository` for consistency.
    - Updated all references to the repository variable in the cidadeRouter functions.

--- simple-api-02/src/routers/cidade-router.ts
+++ simple-api-02/src/routers/cidade-router.ts
@@ -4,17 +4,17 @@ import {diContainer} from "@fastify/awilix";
 
 export default async function cidadeRouter(fastify: FastifyInstance) {
     // Resolve as dependências do container necessárias para as rotas (ou usar request.diContainer para obter scoped instances nas requests)
-    const cidadeRepository = diContainer.resolve("cidadeRepository");
+    const repository = diContainer.resolve("cidadeRepository");
     
     // 1. READ ALL (Listar todas as cidades)
     fastify.get('/', (request, reply) => {
-        return cidadeRepository.list()
+        return repository.list()
     });
 
     // 2. READ BY ID (Buscar uma cidade específica)
     fastify.get<{ Params: { id: string } }>('/:id', async (request, reply) => {
         const id = parseInt(request.params.id);
-        const cidade = await cidadeRepository.get(id);
+        const cidade = await repository.get(id);
         
         if (!cidade) {
             return reply.status(404).send({erro: "Cidade não encontrada"});
@@ -31,14 +31,14 @@ export default async function cidadeRouter(fastify: FastifyInstance) {
             return reply.status(400).send({erro: "Nome e estado são obrigatórios"});
         }
         
-        const cidade = await cidadeRepository.create({nome, estado});
+        const cidade = await repository.create({nome, estado});
         return reply.status(201).send(cidade);
     });
 
     // 4. UPDATE (Atualizar os dados de uma cidade)
     fastify.put<{ Params: { id: string }; Body: Partial<Omit<Cidade, 'id'>> }>('/:id', async (request, reply) => {
         const id = parseInt(request.params.id);
-        const result = await cidadeRepository.update(id, {...request.body});
+        const result = await repository.update(id, {...request.body});
         switch (result.kind) {
             case "ok": return result.value;
             case "not-found": return reply.status(404).send({erro: "Cidade não encontrada."});
@@ -48,7 +48,7 @@ export default async function cidadeRouter(fastify: FastifyInstance) {
     // 5. DELETE (Remover uma cidade)
     fastify.delete<{ Params: { id: string } }>('/:id', async (request, reply) => {
         const id = parseInt(request.params.id);
-        const result = await cidadeRepository.delete(id);
+        const result = await repository.delete(id);
         switch (result.kind) {
             case "ok": return {code: "ok"};
             case "not-found": return {code: "not-found"};
