ec98615099ff56142060a8e76f44a5a7061fbb7a
Fábio Naspolini <fabionaspolini@gmail.com>
2026-05-19 10:49:50 -0300

    sample-api-02: add postgresql db

--- simple-api-02/src/app.ts
+++ simple-api-02/src/app.ts
@@ -1,8 +1,16 @@
 import Fastify from 'fastify';
 import cidadeRouter from "./routers/cidade-router.js";
+import {container, type ContainerCradle} from "./container.js";
 
 const fastify = Fastify({ logger: true });
 fastify.register(cidadeRouter, { prefix: "/cidades" })
+
+// Hook do Fastify para fechar a conexão com o banco de forma segura quando o app desligar
+fastify.addHook('onClose', async () => {
+    const { prisma } = container.cradle as ContainerCradle;
+    await prisma.$disconnect();
+    console.log('Conexão com o PostgreSQL encerrada com sucesso.');
+});
 
 // Inicialização do Servidor
 const start = async () => {
